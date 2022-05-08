5811633b7c4b3a1493cf23b19102068154a8e93a
Richelle Louise Tobias <100325664+RichelleTobias@users.noreply.github.com>
2022-05-08 20:33:27 +0800

    Add files via upload

---
+++ Tobias Project Sprint 3 Output/IceCreamConeClass.py
@@ -0,0 +1,12 @@
+"""
+Program: IceCreamConeClass.py
+Created by Richelle Louise Tobias (GROUP 6)
+Class IceCreamCone for Ice Cream Vending Machine Project
+"""
+
+class IceCreamCone:
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
